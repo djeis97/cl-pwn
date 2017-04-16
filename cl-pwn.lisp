52b7e8f5097690c3e206336df39a10690cfbcfc7
Elijah Malaby <qwe12345678910@gmail.com>
2017-04-16 17:51:11 -0400

    Added simple #'process function.

--- cl-pwn.lisp
+++ cl-pwn.lisp
@@ -11,6 +11,16 @@
     (make-instance 'tube :stream sock
                          :external-format (make-external-format external-format))))
 
+(defun process (command &rest args &key (external-format :utf-8))
+  (let ((proc (apply 'uiop:launch-program command :input :stream
+                                                  :output :stream
+                                                  :error :output
+                                                  :element-type '(unsigned-byte 8)
+                                                  args)))
+    (make-instance 'tube :stream (make-two-way-stream (uiop:process-info-output proc)
+                                                      (uiop:process-info-input proc))
+                         :external-format (make-external-format external-format))))
+
 
 ;;; Timeouts
 
