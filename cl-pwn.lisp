83ff33cd5916a383e74642effbd589956cdb21cd
Elijah Malaby <qwe12345678910@gmail.com>
2017-05-06 14:55:07 -0400

    Corrected #'process and #'remote

--- cl-pwn.lisp
+++ cl-pwn.lisp
@@ -8,8 +8,9 @@
   (let ((sock (socket-stream
                (apply 'socket-connect host port :element-type '(unsigned-byte 8)
                       args))))
-    (make-instance 'tube :stream sock
-                         :external-format (make-external-format external-format))))
+    (make-instance 'flexi-io-tube
+                   :stream sock
+                   :external-format (make-external-format external-format))))
 
 (defun process (command &rest args &key (external-format :utf-8))
   (let ((proc (apply 'uiop:launch-program command :input :stream
@@ -17,9 +18,11 @@
                                                   :error :output
                                                   :element-type '(unsigned-byte 8)
                                                   args)))
-    (make-instance 'tube :stream (make-two-way-stream (uiop:process-info-output proc)
-                                                      (uiop:process-info-input proc))
-                         :external-format (make-external-format external-format))))
+    (make-instance 'process-tube
+                   :stream (make-two-way-stream (uiop:process-info-output proc)
+                                                (uiop:process-info-input proc))
+                   :process proc
+                   :external-format (make-external-format external-format))))
 
 
 ;;; tubes
